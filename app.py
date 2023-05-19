9b335d7a8f7f27e26e14b01264e775e13f6cd229
keisuke-a <36350477+keisuke-a@users.noreply.github.com>
2023-05-19 16:29:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -47,6 +47,6 @@ if st.session_state["messages"]:
     for message in reversed(messages[1:]):  # 直近のメッセージを上に
         speaker = "🙂"
         if message["role"]=="assistant":
-            speaker="🤖"
+            speaker="🔮"
 
         st.write(speaker + ": " + message["content"])
